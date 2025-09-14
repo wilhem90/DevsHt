489d62e97b93daddebf85ee24fe8a208fb39d47b
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2025-09-14 20:44:47 -0300

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,6 +5,9 @@ import Transactions from "../../components/transactions/Transactions";
 import "./Home.css";
 import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    document.title = "Home"
+  }, [])  
   return (
     <div className="container-home">
       <Sidebar />
